29643183b90e2d5228326681c92ab3c2d54dd074
Farrel Satriano <sherrifraynor2004@gmail.com>
2025-05-06 10:19:07 +0700

    penambahan Dashboard.jsx dan folder components yang berisi navbar dan footer

---
+++ React/src/components/footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="bg-gray-300 py-6 text-center">
+      <div className="container mx-auto">
+        <p className="text-gray-800">footer</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
